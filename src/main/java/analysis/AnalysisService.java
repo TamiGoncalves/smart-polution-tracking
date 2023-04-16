21ca89b566e47a5d479068607d89c6abafbb7210
TamiGoncalves <set>
2023-04-16 17:34:37 +0300

    analysis service;

--- src/main/java/analysis/AnalysisService.java
+++ src/main/java/analysis/AnalysisService.java
@@ -1,2 +1,31 @@
-package analysis;public class AnalysisService {
+package analysis;
+
+import io.grpc.stub.StreamObserver;
+
+public class AnalysisService extends DataAnalysisServiceGrpc.DataAnalysisServiceImplBase {
+    /**
+     * @param request
+     * @param responseObserver
+     */
+    @Override
+    public void receiveSensorsData(Analysis.SensorData request, StreamObserver<Analysis.Empty> responseObserver) {
+        super.receiveSensorsData(request, responseObserver);
+    }
+
+    /**
+     * @param request
+     * @param responseObserver
+     */
+    @Override
+    public void updateEnvironmentalPage(Analysis.EnvironmentalData request, StreamObserver<Analysis.Empty> responseObserver) {
+        super.updateEnvironmentalPage(request, responseObserver);
+    }
+
+    /**
+     * @param responseObserver
+     */
+    @Override
+    public StreamObserver<Analysis.DataSensor> updateSensorStatus(StreamObserver<Analysis.SensorStatus> responseObserver) {
+        return super.updateSensorStatus(responseObserver);
+    }
 }
