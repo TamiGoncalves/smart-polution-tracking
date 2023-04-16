98350e1a38f0d84de56d30e0584457192dd08c4a
TamiGoncalves <set>
2023-04-16 17:28:50 +0300

    service discovery

--- src/main/java/hooks/ServiceDiscoListener.java
+++ src/main/java/hooks/ServiceDiscoListener.java
@@ -1,8 +1,12 @@
 package hooks;
+import javax.jmdns.ServiceEvent;
 
-public interface SampleListener{
+public interface ServiceDiscoListener {
 
         public void serviceAdded(ServiceEvent event);
         public void serviceRemoved(ServiceEvent event) ;
         public void serviceResolved(ServiceEvent event) ;
     }
+
+
+    TamiGoncalves <130370756+TamiGoncalves@users.noreply.github.com>
