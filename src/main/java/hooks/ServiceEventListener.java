e4e4c55006b343dec7fa4be50f93d22485e295e0
TamiGoncalves <set>
2023-04-16 17:28:29 +0300

    service listener

--- src/main/java/hooks/ServiceEventListener.java
+++ src/main/java/hooks/ServiceEventListener.java
@@ -1,6 +1,6 @@
 package hooks;
 
-public interface ServiceListener {
+public interface ServiceEventListener {
     public void onAirQualitySend(Object[] data);
     public void onWaterQualitySend(Object[] data);
     void onAnalysis(Object[][] data);
