7d5bc8661fb099dc8b375ca302cf830938071c43
TamiGoncalves <set>
2023-04-16 17:29:44 +0300

    main class

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,22 @@
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
-import java.net.MalformedURLException;
-import java.rmi.Naming;
-import java.rmi.NotBoundException;
-import java.rmi.RemoteException;
+import ui.App;
+
+import javax.swing.*;
 
 public class Main {
 
     public static void main(String[] args) {
         //   Hello object = (Hello) Naming.lookup("//"+hostname+"/HelloServer");
+
+        try {
+            UIManager.setLookAndFeel(
+                    UIManager.getSystemLookAndFeelClassName());
+        }
+        catch(Exception ignored) {
+
+        }
+
         new App();
     }
 
